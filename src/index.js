0b83f3f252e43b555f07e90ff449ade3d00b82b2
Dan Retegan <danretegan@yahoo.com>
2024-06-22 10:37:36 +0300

    Tabbed Content

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import Tooltip from "./ui-library/tooltip.js";
 import Dropdown from "./ui-library/dropdown.js";
+import Tabs from "./ui-library/tabs.js";
 
 console.log("UI Library!");
 
 //* create a tooltip:
 const tooltip = new Tooltip(document.querySelector(".tooltip"));
-
 tooltip.init();
 
 //* create dropdowns (2):
@@ -15,3 +15,7 @@ dropdowns.forEach((elem) => {
   const instance = new Dropdown(elem);
   instance.init();
 });
+
+//* create tabs:
+const tabs = new Tabs(document.querySelector(".tabs"));
+tabs.init();
